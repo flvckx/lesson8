d1462ac75c02c4846c1bd1ecdd390aeb0279d2e5
flvckx <eqwem@ukr.net>
2015-12-20 16:23:56 +0200

    4

--- task2/task2/task2/ViewController.m
+++ task2/task2/task2/ViewController.m
@@ -11,6 +11,7 @@
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *label;
+@property (nonatomic, readonly) NSString *labelText;
 
 @end
 
@@ -19,10 +20,26 @@
 - (IBAction)addButtonTapped:(id)sender {
     NSMutableDictionary *attributesDictionary = [NSMutableDictionary dictionary];
     [attributesDictionary setObject:[UIFont systemFontOfSize:arc4random() % 25] forKey:NSFontAttributeName];
+    [attributesDictionary setObject:[UIColor orangeColor] forKey:NSForegroundColorAttributeName];
     
-    NSAttributedString *newParagraph = [[NSMutableAttributedString alloc] initWithString:@"Paragraph 3" attributes:attributesDictionary];
+    NSMutableAttributedString *newParagraph = [[NSMutableAttributedString alloc] initWithAttributedString:self.label.attributedText];
     
-    self.label.text = [self.label.text stringByAppendingString:newParagraph];
+    NSMutableAttributedString *appending = [[NSMutableAttributedString alloc] initWithString:@"\nParagraph 3" attributes:attributesDictionary];
+    
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.alignment = arc4random() % 5;
+    paragraphStyle.lineSpacing = arc4random() % 5;
+    paragraphStyle.lineBreakMode = arc4random() % 5;
+    
+    NSRange strRange = NSMakeRange(0, newParagraph.length);
+    
+    [newParagraph addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:strRange];
+    
+    NSMutableAttributedString *labelContent = [[NSMutableAttributedString alloc]
+                                               initWithAttributedString:self.label.attributedText];
+    [labelContent appendAttributedString:appending];
+    self.label.attributedText = labelContent;
+    
 }
 
 - (IBAction)clearButtonTapped:(id)sender {
@@ -32,6 +49,27 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)deviceOrientationDidChangeNotification:(NSNotification*)notification {
+    
+    if (UIDeviceOrientationIsPortrait([[UIDevice currentDevice] orientation])) {
+        NSString *substring = [[NSString alloc] init];
+        
+        if ([self.label.text length] >= 200) {
+            substring = [self.label.text substringToIndex:199];
+        } else {
+            substring = self.label.text;
+        }
+        
+        NSAttributedString *text = [[NSAttributedString alloc] initWithString:substring
+                                                                                 attributes:@{NSStrikethroughStyleAttributeName:[NSNumber numberWithInteger:NSUnderlineStyleNone]}];
+        [self.label setAttributedText:text];
+    }
+    
+    if (UIDeviceOrientationIsLandscape([[UIDevice currentDevice] orientation])) {
+        self.label.attributedText = self.label.text;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
